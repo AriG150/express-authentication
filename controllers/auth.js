bbb5dd03000a8b99850015e17ac2a83354145e43
Ari <gryphon150@gmail.com>
2019-10-17 09:31:23 -0700

    created auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,50 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = requires('../config/ppConfig');
+const db = require('../models');
 
+//show signup form
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
 
+router.post('/signup', function(req, res){
+  // Find or create the user 
+  db.user.findOrCreate({
+    where: {email: req.body.email},
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+  .then(function([user, created]) {
+    if ( created) {
+      // we created it, redirect to home 
+        console.log(`🍕 User successfully created`);
+        res.redirect('/')
+    } else {
+      //If user existed, error and reidrect to signup 
+        console.log(`🍥 Email already exists`);
+        res.redirect('/auth/signup');
+    }
+  }).catch(function(err) {
+    //catch any errors
+      console.log(`🚨 ${err}`);
+      res.redirect('/auth/signup');
+  });
+});
+
+
 router.get('/login', function(req, res) {
   res.render('auth/login');
 });
+
+
+//POST form for login
+router.post('/login', passport.authenticat('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login'
+}));
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const session = require('express-session');
 const app = express();
+const passport = require('./config/ppConfig');
 
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static('public'));
 app.use(ejsLayouts);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true 
+}));
+
+//These two lines must come after we set up the session
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -20,6 +32,6 @@ app.get('/profile', function(req, res) {
 
 app.use('/auth', require('./controllers/auth'));
 
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3001, () => console.log(`🎧 You're listening to Port 3001🎧`));
 
 module.exports = server;
